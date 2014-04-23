327ef20f12ca1fc48cc3070ba395dac7928aa52b
Daryll Santos <daryll.santos@gmail.com>
2014-04-23 14:48:07 +0800

    add proper data binding (ng-book p28)

--- app/assets/javascripts/angulearn.js
+++ app/assets/javascripts/angulearn.js
@@ -1,6 +1,12 @@
-function MyController($scope) { var updateClock = function() {
-  $scope.clock = new Date(); };
-  setInterval(function() { $scope.$apply(updateClock);
+function MyController($scope) {
+  $scope.clock = {
+    now: new Date()
+  };
+  var updateClock = function(){
+    $scope.clock.now = new Date()
+  };
+  setInterval(function(){
+    $scope.$apply(updateClock);
   }, 1000);
   updateClock();
 };
