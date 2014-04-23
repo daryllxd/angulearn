552bcbd3ca0442673adcfda9b7d6278728392c15
Daryll Santos <daryll.santos@gmail.com>
2014-04-23 12:21:13 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Angulearn::Application.config.secret_key_base = '94bd30649657bfe3204abba81978edd7b61d7705067ead0923faf7cfe5d38ec8c267e9922d39ad10343d600538e7c94ca0f6a373148c2506934fadcc7c079457'
